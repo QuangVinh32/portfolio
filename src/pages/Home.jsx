8d93b221ec1b994a81547ec6f6eac93329c6afd5
Quang Vinh <quangvinh32@gmail.com>
2025-06-29 20:48:11 +0700

    fix title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,27 +7,22 @@ import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
+import { EducationSection } from "../components/EducationSection";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      {/* Theme Toggle */}
       <ThemeToggle />
-      {/* Background Effects */}
       <StarBackground />
-
-      {/* Navbar */}
       <Navbar />
-      {/* Main Content */}
       <main>
         <HeroSection />
         <AboutSection />
         <SkillsSection />
         <ProjectsSection />
+        <EducationSection />
         <ContactSection />
       </main>
-
-      {/* Footer */}
       <Footer />
     </div>
   );
